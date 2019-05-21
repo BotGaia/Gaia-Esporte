19cdb2e7c4a7eb63981d3d5ac4ffdba13a411883
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2019-05-20 23:54:42 -0300

    Updating README.md
    Co-authored-by: Micaella Gouveia <micaella2212@gmail.com>
    Co-authored-by: Sam Buters <sambuters@gmail.com>

--- src/utils/coordsMathUtil.js
+++ src/utils/coordsMathUtil.js
@@ -1,15 +1,14 @@
 module.exports = {
-    haversine: (lat, lng) => {
-      const EARTH_RADIUS = 6371;
-  
-      const haversineA = Math.sin((lat[1] - lat[0]) / 2) * Math.sin((lat[1] - lat[0]) / 2)
+  haversine: (lat, lng) => {
+    const EARTH_RADIUS = 6371;
+
+    const haversineA = Math.sin((lat[1] - lat[0]) / 2) * Math.sin((lat[1] - lat[0]) / 2)
       + Math.cos(lat[0]) * Math.cos(lat[1]) * Math.sin((lng[1] - lng[0]) / 2)
       * Math.sin((lng[1] - lng[0]) / 2);
-      const haversineB = 2 * Math.atan2(Math.sqrt(haversineA), Math.sqrt(1 - haversineA));
-  
-      return EARTH_RADIUS * haversineB;
-    },
-  
-    toRadians: num => num * (Math.PI / 180),
-  };
-  
+    const haversineB = 2 * Math.atan2(Math.sqrt(haversineA), Math.sqrt(1 - haversineA));
+
+    return EARTH_RADIUS * haversineB;
+  },
+
+  toRadians: num => num * (Math.PI / 180),
+};
--- src/utils/endpointsUtil.js
+++ src/utils/endpointsUtil.js
@@ -1,31 +1,30 @@
 module.exports = {
-    getJson: () => {
-      const endpoints = [
-        {
-          type: 'GET',
-          endpoint: '/local',
-          parameters: [
-            {
-              name: 'address',
-              type: 'string',
-            },
-          ],
-          description: 'Receives a location and return its coordinates',
-        },
-        {
-          type: 'GET',
-          endpoint: '/listLocales',
-          parameters: [
-            {
-              name: 'address',
-              type: 'string',
-            },
-          ],
-          description: 'Receives a location and returns a list with all possible locations',
-        },
-      ];
-  
-      return endpoints;
-    },
-  };
-  
+  getJson: () => {
+    const endpoints = [
+      {
+        type: 'GET',
+        endpoint: '/local',
+        parameters: [
+          {
+            name: 'address',
+            type: 'string',
+          },
+        ],
+        description: 'Receives a location and return its coordinates',
+      },
+      {
+        type: 'GET',
+        endpoint: '/listLocales',
+        parameters: [
+          {
+            name: 'address',
+            type: 'string',
+          },
+        ],
+        description: 'Receives a location and returns a list with all possible locations',
+      },
+    ];
+
+    return endpoints;
+  },
+};
