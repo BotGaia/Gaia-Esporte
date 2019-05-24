3a21f08d94995e16571ba16f485fb79bffd74ae5
Micaella <micaella2212@gmail.com>
2019-05-24 19:16:13 -0300

    HOTFIX #203 Fixing mongo
    Co-authored-by: Sofia Patrocinio <sofiapatrocinio@hotmail.com>

---
+++ src/config/environment.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+module.exports = {
+
+  configSport: () => {
+    const options = {
+      keepAlive: true,
+      socketTimeoutMS: 540000,
+      autoReconnect: true,
+      reconnectTries: Number.MAX_VALUE,
+      autoIndex: false,
+      reconnectInterval: 500,
+      poolSize: 10,
+      bufferMaxEntries: 0,
+      useNewUrlParser: true,
+    };
+    if (process.env.ENVIRONMENT === 'dev') {
+      mongoose.connect('mongodb://mongoesporte:27017/gaiaesporte', options).then(() => {
+      }).catch();
+    } else if (process.env.ENVIRONMENT === 'homolog') {
+      mongoose.connect(`mongodb://${process.env.USER_DB}:${process.env.PASS_DB}@35.225.250.253/${process.env.DB}`,
+        { useNewUrlParser: true }).then(() => {
+      }).catch();
+    }
+  },
+};
