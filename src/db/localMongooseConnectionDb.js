b337587eeb16590f92d1097e7de4e955c4c2eb5c
CalebeRios <calebersmendes@gmail.com>
2019-05-24 18:04:13 -0300

    Fix connection with mongo for authority

--- src/db/localMongooseConnectionDb.js
+++ src/db/localMongooseConnectionDb.js
@@ -16,7 +16,7 @@ module.exports = {
     };
 
     if (process.env.ENVIRONMENT === 'dev') {
-      mongoose.connect('mongodb://mongoesporte:27017/gaiaesporte', options).then(() => {
+      mongoose.connect('mongodb://mongo:27017/gaiaesporte', options).then(() => {
       }).catch();
     } else if (process.env.ENVIRONMENT === 'homolog') {
       mongoose.connect(`mongodb://${process.env.USER_DB}:${process.env.PASS_DB}@35.225.250.253/${process.env.DB}`,
