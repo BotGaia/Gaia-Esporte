f4e178ecc12c0304f81c4154401b40f3a48629f1
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2019-05-28 00:43:30 -0300

    Hotfix #204 Fixing sportForecast route
    Co-authored-by: Micaella Gouveia <micaella2212@gmail.com>

--- src/utils/compareSportWithWeatherUtil.js
+++ src/utils/compareSportWithWeatherUtil.js
@@ -3,92 +3,100 @@ const SportSchema = require('../schemas/sportSchema');
 
 const SportModel = mongoose.model('SportModel', SportSchema);
 
-module.exports = {
-  compare(weather) {
-    return new Promise((resolve) => {
-      this.getAllSports().then((array) => {
-        resolve(this.divideRecommendationArrays(array, weather));
-      });
+function compare(weather) {
+  return new Promise((resolve) => {
+    this.getAllSports().then((array) => {
+      resolve(this.divideRecommendationArrays(array, weather));
     });
-  },
+  });
+}
 
-  divideRecommendationArrays(array, weather) {
-    let count = 0;
-    const objectOfSports = { favorable: [], reservation: [], alert: [] };
-    for (let i = 0; i < array.length; i += 1) {
-      if (this.compareTemperature(array[i], weather)) {
-        count += 1;
-      }
-      if (this.compareHumidity(array[i], weather)) {
-        count += 1;
-      }
-      if (this.compareWindSpeed(array[i], weather)) {
-        count += 1;
-      }
-      if (count === 3) {
-        objectOfSports.favorable.push(array[i]);
-      } else if (count === 2) {
-        objectOfSports.reservation.push(array[i]);
-      } else if (count === 1) {
-        objectOfSports.alert.push(array[i]);
-      }
-      count = 0;
+function divideRecommendationArrays(array, weather) {
+  let count = 0;
+  const objectOfSports = { favorable: [], reservation: [], alert: [] };
+  for (let i = 0; i < array.length; i += 1) {
+    if (this.compareTemperature(array[i], weather)) {
+      count += 1;
     }
-    return objectOfSports;
-  },
+    if (this.compareHumidity(array[i], weather)) {
+      count += 1;
+    }
+    if (this.compareWindSpeed(array[i], weather)) {
+      count += 1;
+    }
+    if (count === 3) {
+      objectOfSports.favorable.push(array[i]);
+    } else if (count === 2) {
+      objectOfSports.reservation.push(array[i]);
+    } else if (count === 1) {
+      objectOfSports.alert.push(array[i]);
+    }
+    count = 0;
+  }
+  return objectOfSports;
+}
 
-  getAllSports() {
-    return new Promise((resolve) => {
-      SportModel.find({ class: 'sport' }).then((array) => {
-        resolve(array);
-      }).catch(() => {
-      });
+function getAllSports() {
+  return new Promise((resolve) => {
+    SportModel.find({ class: 'sport' }).then((array) => {
+      resolve(array);
+    }).catch(() => {
     });
-  },
+  });
+}
 
-  compareWeather: (sport, weather) => {
-    let recommendationLevel = 0;
-    if (this.compareHumidity(sport, weather)) {
-      recommendationLevel += 1;
-    }
-    if (this.compareTemperature(sport, weather)) {
-      recommendationLevel += 1;
-    }
-    if (this.compareWindSpeed(sport, weather)) {
-      recommendationLevel += 1;
-    }
+function compareWeather(sport, weather) {
+  let recommendationLevel = 0;
+  if (this.compareHumidity(sport, weather)) {
+    recommendationLevel += 1;
+  }
+  if (this.compareTemperature(sport, weather)) {
+    recommendationLevel += 1;
+  }
+  if (this.compareWindSpeed(sport, weather)) {
+    recommendationLevel += 1;
+  }
 
-    return recommendationLevel;
-  },
+  return recommendationLevel;
+}
 
-  compareTemperature: (sport, weather) => {
-    for (let i = 0; i < sport.temperature.length; i += 1) {
-      if (parseFloat(weather.temperature) >= parseFloat(sport.temperature[i].lowerLimit)
+function compareTemperature(sport, weather) {
+  for (let i = 0; i < sport.temperature.length; i += 1) {
+    if (parseFloat(weather.temperature) >= parseFloat(sport.temperature[i].lowerLimit)
       && parseFloat(weather.temperature)
       <= parseFloat(sport.temperature[i].upperLimit)) {
-        return true;
-      }
+      return true;
     }
-    return false;
-  },
+  }
+  return false;
+}
 
-  compareHumidity: (sport, weather) => {
-    for (let i = 0; i < sport.humidity.length; i += 1) {
-      if (parseFloat(weather.humidity) >= parseFloat(sport.humidity[i].lowerLimit)
+function compareHumidity(sport, weather) {
+  for (let i = 0; i < sport.humidity.length; i += 1) {
+    if (parseFloat(weather.humidity) >= parseFloat(sport.humidity[i].lowerLimit)
       && parseFloat(weather.humidity) <= parseFloat(sport.humidity[i].upperLimit)) {
-        return true;
-      }
+      return true;
     }
-    return false;
-  },
+  }
+  return false;
+}
 
-  compareWindSpeed: (sport, weather) => {
-    for (let i = 0; i < sport.windSpeed.length; i += 1) {
-      if (parseFloat(weather.windySpeed) >= parseFloat(sport.windSpeed[i].lowerLimit)
+function compareWindSpeed(sport, weather) {
+  for (let i = 0; i < sport.windSpeed.length; i += 1) {
+    if (parseFloat(weather.windySpeed) >= parseFloat(sport.windSpeed[i].lowerLimit)
       && parseFloat(weather.windySpeed) <= parseFloat(sport.windSpeed[i].upperLimit)) {
-        return true;
-      }
+      return true;
     }
-    return false;
-  },
+  }
+  return false;
+}
+
+module.exports = {
+  compare,
+  divideRecommendationArrays,
+  getAllSports,
+  compareWeather,
+  compareTemperature,
+  compareHumidity,
+  compareWindSpeed,
 };
