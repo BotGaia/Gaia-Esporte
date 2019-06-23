acc9893f1d00e189f604efc36045ec1bff62aca4
Sam Buters <sambuters@gmail.com>
2019-06-23 14:31:01 -0300

    Increasing treatTimeUtil coverage

--- src/test/treatTimeTest.js
+++ src/test/treatTimeTest.js
@@ -2,15 +2,24 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 const chai = require('chai');
+const sinon = require('sinon');
 const treatTime = require('../utils/treatTimeUtil');
 
 const should = chai.should();
 
 describe('Treat Time', () => {
-  it('Should get date time', (done) => {
-    const time = treatTime.getDateTime();
-    time.should.not.be.a('Float');
+  it('should get date time', (done) => {
+    const today = new Date(1561309569565);
+    today.setHours(0);
+    const fakeClock = sinon.useFakeTimers(today.getTime());
+
+    for (let counter = 0; counter < 3; counter += 1) {
+      treatTime.getDateTime().should.be.a('Number').that.is.equal(today.getDay() - 1);
+      fakeClock.tick('01:00:00');
+    }
+
     done();
+    fakeClock.restore();
   }).timeout(5000);
 
   it('Should get date info', (done) => {
