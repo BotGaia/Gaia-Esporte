20501d28b981e0ddec1c627c7cf7980e81576d16
Sam Buters <sambuters@gmail.com>
2019-06-23 18:04:55 -0300

    Increasing test coverage for multiple files

--- src/test/treatNotificationTest.js
+++ src/test/treatNotificationTest.js
@@ -4,23 +4,31 @@
 const chai = require('chai');
 const treatNotification = require('../utils/treatNotificationUtil');
 
-const mockJson = {
-  telegramId: 'testId38',
-  sport: 'testSport',
-  days: [8],
-  hour: 12,
-  minutes: 30,
-  locals: 'Fabrica do Papai Noel',
-  hoursBefore: 0,
-  minutesBefore: 15,
-};
 
-describe('Save notification', () => {
-  it('Save notification', (done) => {
-    treatNotification.saveNotification(mockJson).then((notification) => {
+describe('Notification', () => {
+  it('should save a notification', (done) => {
+    const requestBody = {
+      telegramId: 'testId38',
+      sport: 'testSport',
+      days: [8],
+      hour: 12,
+      minutes: 30,
+      locals: 'Fabrica do Papai Noel',
+      hoursBefore: 0,
+      minutesBefore: 15,
+    };
+
+    treatNotification.saveNotification(requestBody).then((notification) => {
       notification.getTelegramId().should.eql('testId38');
       notification.getSport().should.eql('testSport');
       done();
     });
   }).timeout(5000);
+
+  it('should get all notifications', (done) => {
+    treatNotification.getAllNotifications().then((notifications) => {
+      notifications.should.be.a('Array');
+      done();
+    });
+  });
 });
